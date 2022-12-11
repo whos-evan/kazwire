15eac118cc54b3b2f6f879fe96b8bff4d94941b0
Biune <you@example.com>
2022-12-11 12:58:18 -0600

    added recommended games

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	import Nav from './nav.svelte';
 	import Promo from './promo.svelte';
+	import RandomBox from './randomBox.svelte';
 </script>
 
 <Nav />
 <Promo />
+<RandomBox />
---
+++ src/routes/randomBox.svelte
@@ -0,0 +1,29 @@
+<script>
+	import Box from './games/box.svelte';
+	import gamesJson from './games/games.json';
+
+	let randomGame1 = gamesJson['games'][Math.floor(Math.random() * gamesJson['games'].length)];
+	let randomGame2 = gamesJson['games'][Math.floor(Math.random() * gamesJson['games'].length)];
+</script>
+
+<div class="text-white w-full pl-10 pr-10 pt-5 text-center">
+	<h1 class="lg:text-5xl md:text-4xl sm:text-xl font-bold mb-5">
+		Here are two recommended games!
+	</h1>
+	<div
+		class="grid grid-flow-rows grid-cols-2 auto-rows-auto gap-10"
+	>
+		<Box
+			title={randomGame1['name']}
+			image={randomGame1['image']}
+			description={randomGame1['description']}
+			id={randomGame1['id']}
+		/>
+		<Box
+			title={randomGame2['name']}
+			image={randomGame2['image']}
+			description={randomGame2['description']}
+			id={randomGame2['id']}
+		/>
+	</div>
+</div>
