81451670e69b6f846ed31bf8e1b87d28a84448e3
Biune <you@example.com>
2022-12-11 17:13:44 -0600

    added emulator.js and games and script to add games easily

--- add-game.py
+++ add-game.py
@@ -95,6 +95,46 @@ else:
         write_json(gameJson, './src/routes/games/games.json')
         
         urllib.request.urlretrieve(image, "./static/games/img/" + imageFileName)
+    elif input('Is the game a n64 game? (yes/no) ') == 'yes':
+        import shutil, errno
+
+        def copyanything(src, dst):
+            try:
+                shutil.copytree(src, dst)
+            except OSError as exc: # python >2.5
+                if exc.errno in (errno.ENOTDIR, errno.EINVAL):
+                    shutil.copy(src, dst)
+                else: raise
+
+        copyanything('./static/games/n64-template', './static/games/' + id)
+
+        with open(r'./static/games/' + id + '/index.html', 'r') as file:
+  
+            # Reading the content of the file
+            # using the read() function and storing
+            # them in a new variable
+            data = file.read()
+
+            data = data.replace('game-url-here', id)
+
+        with open(r'./static/games/' + id + '/index.html', 'w') as file:
+  
+            # Writing the replaced data in our
+            # text file
+            file.write(data)
+
+        print('Adding game', game, 'with ID', id, 'and image', image, 'and developer', developer, 'and description', description, 'and isEmbed', isEmbed)
+
+        gameJson = {
+            'name': game,
+            'id': id,
+            'image': imageFileName,
+            'developer': developer,
+            'description': description
+        }
+        write_json(gameJson, './src/routes/games/games.json')
+        
+        urllib.request.urlretrieve(image, "./static/games/img/" + imageFileName)
     else:
         isEmbed = 'no'
         print('Adding game', game, 'with ID', id, 'and image', image, 'and developer', developer, 'and description', description, 'and isEmbed', isEmbed)
