397671a92055dc0f14bd79551f00ed11485c1088
evan_ <72959444+whos-evan@users.noreply.github.com>
2022-12-15 11:32:42 -0600

    fuck me

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script>
-	import Nav from './components/nav.svelte';
+	import Nav from './nav.svelte';
 	import Promo from './promo.svelte';
 	import RandomBox from './randomBox.svelte';
 	import News from './news.svelte';
-	import Footer from './components/footer.svelte';
+	import Footer from './footer.svelte';
 </script>
 
 <Nav />
--- src/routes/discord/+page.svelte
+++ src/routes/discord/+page.svelte
@@ -1,7 +1,7 @@
 <script>
-	import Nav from '../components/nav.svelte';
+	import Nav from '../nav.svelte';
 	import Discord from './discord.svelte';
-	import Footer from '../components/footer.svelte';
+	import Footer from '../footer.svelte';
 </script>
 
 <Nav />
