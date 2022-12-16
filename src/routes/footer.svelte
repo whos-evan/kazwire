d8c8fa0414b222d8cb6e86e3f90ee621b069016c
Biune <you@example.com>
2022-12-15 22:54:33 -0600

    pretty up legal

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -1,11 +1,24 @@
+
+<script lang="ts">
+    import { darkMode } from "../dark";
+    import { onMount } from "svelte";
+    onMount(() => {
+        darkMode();
+        console.log('test')
+    });
+    
+</script>
+
 <div class="Footer">
     <nav class="bg-[#0875bb] max-w mt-10 pt-2">
         <div class="flex items-center justify-evenly flex-wrap p-6">
             <div class="flex items-center text-white ml-10">
                 <ul class="align-middle leading-[3rem]">
-                    <li class='float-left'><a href="/tos.html" class="text-white text-lg">Terms of Service</a></li>
-                    <li class='float-left'><a href="/privacy-policy.html" class="text-white text-lg ml-10">Privacy Policy</a></li>
-                    <li class='float-left'><a href="/dmca.html" class="text-white text-lg ml-10">DMCA</a></li>
+                    <li class='float-left text-white text-lg'>Made with ❤️ by evan</li>
+                    <li class='float-left'><a href="/legal/tos" class="text-white text-lg ml-10">Terms of Service</a></li>
+                    <li class='float-left'><a href="/legal/privacy-policy" class="text-white text-lg ml-10">Privacy Policy</a></li>
+                    <li class='float-left'><a href="/legal/dmca" class="text-white text-lg ml-10">DMCA</a></li>
+                    <li class='float-left text-white text-lg ml-10'>©2022 Kazwire</li>
                 </ul>
         </div>
     </nav>
---
+++ src/routes/legal/dmca/+page.svelte
@@ -0,0 +1,160 @@
+<script>
+	import Footer from '../../footer.svelte';
+	import Nav from '../../nav.svelte';
+</script>
+
+<Nav />
+
+<div id="home" class="h-full w-full pl-10 pt-10 pr-10 text-white">
+	<h1 class="text-3xl">DMCA policy</h1>
+	<br>
+	<p>
+		This Digital Millennium Copyright Act policy (&#8220;Policy&#8221;) applies to the <a
+			href="https://kazwire.com">kazwire.com</a
+		> website (&#8220;Website&#8221; or &#8220;Service&#8221;) and any of its related products and services
+		(collectively, &#8220;Services&#8221;) and outlines how this Website operator (&#8220;Operator&#8221;,
+		&#8220;we&#8221;, &#8220;us&#8221; or &#8220;our&#8221;) addresses copyright infringement notifications
+		and how you (&#8220;you&#8221; or &#8220;your&#8221;) may submit a copyright infringement complaint.
+	</p>
+	<br>
+	<p>
+		Protection of intellectual property is of utmost importance to us and we ask our users and their
+		authorized agents to do the same. It is our policy to expeditiously respond to clear
+		notifications of alleged copyright infringement that comply with the United States Digital
+		Millennium Copyright Act (&#8220;DMCA&#8221;) of 1998, the text of which can be found at the
+		U.S. Copyright Office <a
+			href="https://www.copyright.gov"
+			target="_blank"
+			rel="nofollow noreferrer noopener external">website</a
+		>. This DMCA policy was created with the help of
+		<a href="https://www.websitepolicies.com" target="_blank" rel="nofollow">WebsitePolicies</a>.
+	</p>
+	<br>
+	<div class="wpembed-index">
+		<h3 class="text-2xl">Table of contents</h3>
+		<br>
+		<ol class="wpembed-index">
+			<li>
+				<a href="#what-to-consider-before-submitting-a-copyright-complaint"
+					>What to consider before submitting a copyright complaint</a
+				>
+			</li>
+			<li><a href="#notifications-of-infringement">Notifications of infringement</a></li>
+			<li><a href="#counter-notifications">Counter-notifications</a></li>
+			<li><a href="#changes-and-amendments">Changes and amendments</a></li>
+			<li><a href="#reporting-copyright-infringement">Reporting copyright infringement</a></li>
+		</ol>
+	</div>
+	<br>
+	<h2 class="text-2xl" id="what-to-consider-before-submitting-a-copyright-complaint">
+		What to consider before submitting a copyright complaint
+	</h2>
+	<br>
+	<p>
+		Before submitting a copyright complaint to us, consider whether the use could be considered fair
+		use. Fair use states that brief excerpts of copyrighted material may, under certain
+		circumstances, be quoted verbatim for purposes such as criticism, news reporting, teaching, and
+		research, without the need for permission from or payment to the copyright holder. If you have
+		considered fair use, and you still wish to continue with a copyright complaint, you may want to
+		first reach out to the user in question to see if you can resolve the matter directly with the
+		user.
+	</p>
+	<br>
+	<p>
+		Please note that if you are unsure whether the material you are reporting is in fact infringing,
+		you may wish to contact an attorney before filing a notification with us.
+	</p>
+	<p>
+		We may, at our discretion or as required by law, share a copy of your notification or
+		counter-notification with third parties. This may include sharing the information with the
+		account holder engaged in the allegedly infringing activity or for publication.
+	</p>
+	<br>
+	<h2 class="text-2xl" id="notifications-of-infringement">Notifications of infringement</h2>
+	<br>
+	<p>
+		If you are a copyright owner or an agent thereof, and you believe that any material available on
+		our Services infringes your copyrights, then you may submit a written copyright infringement
+		notification (&#8220;Notification&#8221;) using the contact details below pursuant to the DMCA.
+		All such Notifications must comply with the DMCA requirements.
+	</p>
+	<br>
+	<p>
+		Filing a DMCA complaint is the start of a pre-defined legal process. Your complaint will be
+		reviewed for accuracy, validity, and completeness. If your complaint has satisfied these
+		requirements, our response may include the removal or restriction of access to allegedly
+		infringing material.
+	</p>
+	<br>
+	<p>
+		If we remove or restrict access to materials or terminate an account in response to a
+		Notification of alleged infringement, we will make a good faith effort to contact the affected
+		user with information concerning the removal or restriction of access, which may include a full
+		copy of your Notification (including your name, address, phone, and email address), along with
+		instructions for filing a counter-notification.
+	</p>
+	<br>
+	<p>
+		Notwithstanding anything to the contrary contained in any portion of this Policy, the Operator
+		reserves the right to take no action upon receipt of a DMCA copyright infringement notification
+		if it fails to comply with all the requirements of the DMCA for such notifications.
+	</p>
+	<br>
+	<h2 class="text-2xl" id="counter-notifications">Counter-notifications</h2>
+	<br>
+	<p>
+		A user who receives a copyright infringement Notification may make a counter-Notification
+		pursuant to sections 512(g)(2) and (3) of the US Copyright Act. If you receive a copyright
+		infringement Notification, it means that the material described in the Notification has been
+		removed from our Services or access to the material has been restricted. Please take the time to
+		read through the Notification, which includes information on the Notification we received. To
+		file a counter-notification with us, you must provide a written communication compliant with the
+		DMCA requirements.
+	</p>
+	<br>
+	<p>
+		Please note that if you are not sure whether certain material infringes the copyrights of others
+		or that the material or activity was removed or restricted by mistake or misidentification, you
+		may wish to contact an attorney before filing a counter-notification.
+	</p>
+	<br>
+	<p>
+		Notwithstanding anything to the contrary contained in any portion of this Policy, the Operator
+		reserves the right to take no action upon receipt of a counter-notification. If we receive a
+		counter-notification that complies with the terms of 17 U.S.C. § 512(g), we may forward it to
+		the person who filed the original Notification.
+	</p>
+	<br>
+	<h2 class="text-2xl" id="changes-and-amendments">Changes and amendments</h2>
+	<br>
+	<p>
+		We reserve the right to modify this Policy or its terms related to the Website and Services at
+		any time at our discretion. When we do, we will send you an email to notify you. We may also
+		provide notice to you in other ways at our discretion, such as through the contact information
+		you have provided.
+	</p>
+	<br>
+	<p>
+		An updated version of this Policy will be effective immediately upon the posting of the revised
+		Policy unless otherwise specified. Your continued use of the Website and Services after the
+		effective date of the revised Policy (or such other act specified at that time) will constitute
+		your consent to those changes.
+	</p>
+	<br>
+	<h2 class="text-2xl" id="reporting-copyright-infringement">Reporting copyright infringement</h2>
+	<br>
+	<p>
+		If you would like to notify us of the infringing material or activity, we encourage you to
+		contact us using the details below:
+	</p>
+	<br>
+	<p>
+		<a
+			href="&#109;&#097;&#105;&#108;&#116;&#111;&#058;&#99;&#111;n&#116;a&#99;t&#64;is-&#97;&#46;win"
+			>c&#111;&#110;&#116;&#97;&#99;&#116;&#64;i&#115;-a&#46;win</a
+		>
+	</p>
+	<br>
+	<p>This document was last updated on December 15, 2022</p>
+</div>
+<Footer />
