09d85527a4a6a10c9076cba1b7330cc114e9f7c1
Biune <you@example.com>
2022-12-16 17:00:00 -0600

    nav  is really big, let's make it smaller

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -34,21 +34,21 @@
 
 <div class="Nav">
     <nav class="bg-[#0875bb] max-w mb-5 shadow-md shadow-black dark:shadow-white">
-        <div class="flex items-center justify-between flex-wrap p-6">
+        <div class="flex items-center justify-between flex-wrap p-3">
             <div class="flex items-center text-white ml-10">
-                <ul class="align-middle leading-[4rem]">
+                <ul class="align-middle leading-[2.5rem]">
                     <li class='float-left'>
                         <a class="block" href="/">
-                            <img src="/logo.png" class="h-16" alt="Kazwire Logo"/>
+                            <img src="/logo.png" class="h-10" alt="Kazwire Logo"/>
                         </a>
                     </li>
-                    <li class='float-left'><a href="/search" class="text-white text-lg hover:text-gray-200 ml-10">Search</a></li>
-                    <li class='float-left'><a href="/games" class="text-white text-lg hover:text-gray-200 ml-10">Games</a></li>
-                    <li class='float-left'><a href="/discord" class="text-white hover:text-gray-200 text-lg ml-10">Support/Discord</a></li>
+                    <li class='float-left'><a href="/search" class="text-white text-md hover:text-gray-200 ml-10">Search</a></li>
+                    <li class='float-left'><a href="/games" class="text-white text-md hover:text-gray-200 ml-10">Games</a></li>
+                    <li class='float-left'><a href="/discord" class="text-white hover:text-gray-200 text-md ml-10">Support/Discord</a></li>
                 </ul>
 
             </div>
-                <div class='float-right text-white text-2xl mr-10'>
+                <div class='float-right text-white text-xl mr-10'>
                     <button id="theme-selector" class="bg-[#0875bb] hover:bg-[#0875bb] text-white font-bold py-2 px-4 rounded-full" on:click={toggle}>
                         ☀️
                     </button>
