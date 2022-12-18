e3540869a9c93ad223cb3a20b330bf3beaea203d
Biune <you@example.com>
2022-12-18 16:08:27 -0600

    ads

--- src/components/horz-ad.svelte
+++ src/components/horz-ad.svelte
@@ -1,15 +1,16 @@
 <script>
     import { onMount } from 'svelte';
     onMount(() => {
-      setTimeout(() => {
-            // We are targeting the first banner ad of AdSense
-            var ad = document.querySelector("ins.adsbygoogle"); 
-            // If the ad contains no innerHTML, ad blockers are at work
-            if (ad && ad.innerHTML.replace(/\s/g, "").length == 0) { 
-                document.getElementById('GoogleAds').style.cssText = 'display:block !important; padding: 20px !important;';
-                document.getElementById('GoogleAds').innerHTML = '<img src="/logo.png" width="75"/><p class="text-2xl text-white"></p>';
-            }
-        }, 10000);
+        (adsbygoogle = window.adsbygoogle || []).push({});
+        setTimeout(() => {
+                // We are targeting the first banner ad of AdSense
+                var ad = document.querySelector("ins.adsbygoogle"); 
+                // If the ad contains no innerHTML, ad blockers are at work
+                if (ad && ad.innerHTML.replace(/\s/g, "").length == 0) { 
+                    document.getElementById('GoogleAds').style.cssText = 'display:block !important; padding: 20px !important;';
+                    document.getElementById('GoogleAds').innerHTML = '<img src="/logo.png" width="75"/><p class="text-2xl text-white"></p>';
+                }
+            }, 10000);
     });
 
     
--- src/components/vert-ad.svelte
+++ src/components/vert-ad.svelte
@@ -1,17 +1,18 @@
 <script>
     import { onMount } from 'svelte';
     onMount(() => {
-      setTimeout(() => {
-            // We are targeting the first banner ad of AdSense
-            var ad = document.querySelector("ins.adsbygoogle"); 
-            // If the ad contains no innerHTML, ad blockers are at work
-            // if its unfilled then don't show anything
-            
-            if (ad && ad.innerHTML.replace(/\s/g, "").length == 0) { 
-                document.getElementById('GoogleAds').style.cssText = 'display:block !important; padding: 20px !important;';
-                document.getElementById('GoogleAds').innerHTML = '<img src="/logo.png" width="75"/><p class="text-2xl text-white"></p>';
-            }
-        }, 10000);
+        (adsbygoogle = window.adsbygoogle || []).push({});
+        setTimeout(() => {
+                // We are targeting the first banner ad of AdSense
+                var ad = document.querySelector("ins.adsbygoogle"); 
+                // If the ad contains no innerHTML, ad blockers are at work
+                // if its unfilled then don't show anything
+                
+                if (ad && ad.innerHTML.replace(/\s/g, "").length == 0) { 
+                    document.getElementById('GoogleAds').style.cssText = 'display:block !important; padding: 20px !important;';
+                    document.getElementById('GoogleAds').innerHTML = '<img src="/logo.png" width="75"/><p class="text-2xl text-white"></p>';
+                }
+            }, 10000);
     });
 
     
