1b2551b01d622b16f7e02a73f8881104192354ea
Biune <you@example.com>
2022-12-19 13:37:40 -0600

    search ads as well

--- src/routes/search/search.svelte
+++ src/routes/search/search.svelte
@@ -94,6 +94,7 @@
         searchFrame.style.borderRadius = '0';
     }
 
+    import VertAd from "../../components/vert-ad.svelte";
 </script>
 
 <div class="text-white w-full pb-5 text-center">
@@ -108,28 +109,36 @@
       </form>
 </div>
 
-<div class="flex h-[calc(100vh-220px)] max-w-full pl-5 pr-5 pb-5">
-	<div class="flex-grow mb-14 align-center">
-		<iframe id="search-frame" name="search" class="search w-full h-full rounded-t-lg bg-blue-200 dark:bg-gray-800"/>
-		<div
-			class="block relative items-center h-14 leading-[3.5rem] mt-2 rounded-b-lg bg-[#0875bb] text-white"
-		>
-			<div class="float-right mr-5">
-				<button class="mt-4 h-5 w-5 fill-white" on:click={fullScreen}>
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
-						><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path
-							d="M32 32C14.3 32 0 46.3 0 64v96c0 17.7 14.3 32 32 32s32-14.3 32-32V96h64c17.7 0 32-14.3 32-32s-14.3-32-32-32H32zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7 14.3 32 32 32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H64V352zM320 32c-17.7 0-32 14.3-32 32s14.3 32 32 32h64v64c0 17.7 14.3 32 32 32s32-14.3 32-32V64c0-17.7-14.3-32-32-32H320zM448 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v64H320c-17.7 0-32 14.3-32 32s14.3 32 32 32h96c17.7 0 32-14.3 32-32V352z"
-						/></svg>
-				</button>
-			</div>
-            <div class="float-right mr-5">
-				<button class="mt-[1.1rem] h-5 w-5 fill-white" on:click={maximize}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M344 0H488c13.3 0 24 10.7 24 24V168c0 9.7-5.8 18.5-14.8 22.2s-19.3 1.7-26.2-5.2l-39-39-87 87c-9.4 9.4-24.6 9.4-33.9 0l-32-32c-9.4-9.4-9.4-24.6 0-33.9l87-87L327 41c-6.9-6.9-8.9-17.2-5.2-26.2S334.3 0 344 0zM184 496H40c-13.3 0-24-10.7-24-24V328c0-9.7 5.8-18.5 14.8-22.2s19.3-1.7 26.2 5.2l39 39 87-87c9.4-9.4 24.6-9.4 33.9 0l32 32c9.4 9.4 9.4 24.6 0 33.9l-87 87 39 39c6.9 6.9 8.9 17.2 5.2 26.2s-12.5 14.8-22.2 14.8z"/></svg>
-				</button>
-			</div>
-			<div id="title" class="ml-5">
-                Nothing here...
+<div class="flex flex-row relative justify-center">
+	<div class="flex h-[calc(90vh-132px)] md:w-[80vw] sm:w-full float-left pl-5 pr-5 pb-5">
+		<div class="flex-grow mb-14 align-center">
+            <div class="w-full h-full">
+                <iframe id="search-frame" class="w-full h-full rounded-t-lg bg-black" />
+            </div>
+
+			<div class="grid">
+				<div class="w-full relative items-center h-14 leading-[3.5rem] mt-2 rounded-b-lg bg-[#0875bb] text-white">
+					<div class="float-right mr-5">
+						<button class="mt-4 h-5 w-5 fill-white" on:click={fullScreen}>
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
+								><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path
+									d="M32 32C14.3 32 0 46.3 0 64v96c0 17.7 14.3 32 32 32s32-14.3 32-32V96h64c17.7 0 32-14.3 32-32s-14.3-32-32-32H32zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7 14.3 32 32 32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H64V352zM320 32c-17.7 0-32 14.3-32 32s14.3 32 32 32h64v64c0 17.7 14.3 32 32 32s32-14.3 32-32V64c0-17.7-14.3-32-32-32H320zM448 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v64H320c-17.7 0-32 14.3-32 32s14.3 32 32 32h96c17.7 0 32-14.3 32-32V352z"
+								/></svg>
+						</button>
+					</div>
+					<div class="float-right mr-5">
+						<button class="mt-[1.1rem] h-5 w-5 fill-white" on:click={maximize}>
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M344 0H488c13.3 0 24 10.7 24 24V168c0 9.7-5.8 18.5-14.8 22.2s-19.3 1.7-26.2-5.2l-39-39-87 87c-9.4 9.4-24.6 9.4-33.9 0l-32-32c-9.4-9.4-9.4-24.6 0-33.9l87-87L327 41c-6.9-6.9-8.9-17.2-5.2-26.2S334.3 0 344 0zM184 496H40c-13.3 0-24-10.7-24-24V328c0-9.7 5.8-18.5 14.8-22.2s19.3-1.7 26.2 5.2l39 39 87-87c9.4-9.4 24.6-9.4 33.9 0l32 32c9.4 9.4 9.4 24.6 0 33.9l-87 87 39 39c6.9 6.9 8.9 17.2 5.2 26.2s-12.5 14.8-22.2 14.8z"/></svg>
+						</button>
+					</div>
+					<div id="title" class="ml-5">
+						Nothing here...
+					</div>
+				</div>
 			</div>
 		</div>
+	</div>
+	<div class="w-full max-w-[280px] hidden xl:flex flex-col justify-center items-center gap-y-2 px-3 bg-opacity-50 backdrop-blur-md" align="middle">
+		<VertAd />
 	</div>
 </div>
