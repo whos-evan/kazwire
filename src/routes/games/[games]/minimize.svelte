a94e28ad80084a31b14891765713cc48beb0521e
Biune <you@example.com>
2023-01-01 14:22:02 -0600

    Whoops

---
+++ src/routes/games/[games]/minimize.svelte
@@ -0,0 +1,16 @@
+<button
+	class="m-5 px-4 py-3 text-lg font-extrabold bg-secondary opacity-50 rounded-lg text-white absolute"
+	on:click={() => window.history.back()}
+>
+	<!-- back arrow -->
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 448 512"
+		class="h-6 w-6"
+		fill="white"
+		stroke="currentColor"
+		><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path
+			d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"
+		/></svg
+	>
+</button>
