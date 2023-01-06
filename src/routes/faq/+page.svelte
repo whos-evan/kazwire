501e8ad162048957cefb063cdf8bafde4cc366b2
Biune <you@example.com>
2023-01-06 17:33:20 -0600

    sizing for hidden page

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	import Faq from './faq.svelte';
 	import Nav from '../../components/nav.svelte';
+	import Footer from '../../components/footer.svelte';
 </script>
 
 <Nav />
 <Faq />
+<Footer />
