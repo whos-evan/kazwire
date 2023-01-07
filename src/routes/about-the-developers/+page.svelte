d31d3cd9a63a6e3b667742df52f37d5f44f31e30
zachmb <zachmb2008@gmail.com>
2023-01-06 19:21:10 -0600

    this might fix the previous commit

--- src/routes/about-the-developers/+page.svelte
+++ src/routes/about-the-developers/+page.svelte
@@ -1,15 +1,12 @@
 <script>
 	import Footer from '../../../components/footer.svelte';
+	import about-the-developers from './about-the-developers.svelte';
 	import Nav from '../../../components/nav.svelte';
 </script>
 
 <Nav />
 
-<div id="home" class="h-full w-full pl-10 pt-10 pr-10 text-white">
-<h1 class="text-3xl">About the Developers</h1>
-<p>Evan - Coded this site and did the bulk of the work. He has many years of experience in developing web projects and his other sites and works can be found on <a href="https://github.com/whos-evan?_gl=1*swha64*_ga*ODYwNDIxOTY2LjE2NzE0OTUxODU.*_ga_FLBJ64LGWV*MTY3MzA0NjczMi44MC4xLjE2NzMwNDk3NDUuMC4wLjA" target="_blank">his github</a>.</p>
-<p>Zachary - Made the name and whatnot and promotes the site by making videos. His YouTube channel can be found at <a href="https://www.youtube.com/channel/UCWjGQfoEddPru_KC9zYfLig" target="_blank">Kazwire</a>.</p>
-<p>If you would like to join our team you can apply on our <a href="https://discord.com/invite/5jKVKPtZSu" target="_blank">Discord server</a>!</p>
+<about-the-developers />
 
 </div>
 
