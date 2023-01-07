d5364e56f625929ad687a616c09ac03880a0f7c1
Biune <you@example.com>
2023-01-06 19:39:54 -0600

    capitalization + sizing

--- src/routes/about-the-developers/+page.svelte
+++ src/routes/about-the-developers/+page.svelte
@@ -4,23 +4,25 @@
 </script>
 
 <Nav />
-<div id="home" class="h-full w-full pl-10 pt-10 pr-10 text-white">
+<div id="home" class="h-[80vh] w-full pl-10 pt-10 pr-10 text-white">
 	<h1 class="text-3xl">About the Developers</h1>
-	<p>
+	<br>
+	<p class="text-xl">
 		Evan - Coded this site and did the bulk of the work. He has many years of experience in
 		developing web projects and his other sites and works can be found on <a
 			href="https://github.com/whos-evan"
 			target="_blank">his github</a
 		>.
 	</p>
-	<p>
+	<p class="text-xl">
 		Zachary - Made the name and whatnot and promotes the site by making videos. His YouTube channel
 		can be found at <a
 			href="https://www.youtube.com/channel/UCWjGQfoEddPru_KC9zYfLig"
 			target="_blank">Kazwire</a
 		>.
 	</p>
-	<p>
+	<br>
+	<p class="text-xl">
 		If you would like to join our team you can apply on our <a
 			href="https://discord.com/invite/5jKVKPtZSu"
 			target="_blank">Discord server</a
