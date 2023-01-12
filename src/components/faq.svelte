77270d4e89e75abd0099d6d5844394baf4f1b50c
Biune <you@example.com>
2023-01-12 13:54:40 -0600

    Add credits.

--- src/components/faq.svelte
+++ src/components/faq.svelte
@@ -1,3 +1,20 @@
+<script>
+	import gamesJson from '../routes/games/games.json';
+
+	// go through each game and find the developers and put them in a string
+	let developers = '';
+	gamesJson['games'].forEach((game) => {
+		if (game['developer'] != 'Unknown'){
+			developers += game['developer'] + ', ';
+		}
+	});
+	// remove duplicates
+	developers = [...new Set(developers.split(', '))].join(', ');
+	// remove the last comma and space
+	developers = developers.slice(0, -2);
+	developers = developers.replace(/,([^,]*)$/, ', and$1');
+</script>
+
 <div class="Faq w-full pl-10 pr-10">
 	<div class="text-white grid grid-rows-5 mt-5">
 		<h1 class="text-4xl text-white font-bold row-span-1 text-center mb-5">What is Kazwire?</h1>
@@ -27,6 +44,14 @@
 					class="hover:cursor-pointer hover:underline"
 					href="/legal/dmca">DMCA page</a
 				>.
+			</p>
+		</div>
+		<h1 class="text-4xl text-white font-bold row-span-1 text-center mt-5 mb-5">Credits</h1>
+		<div class="text-xl row-span-4 ml-10 mr-10">
+			<p>
+				We would like to thank the following developers and publishers for their games:
+				<br>
+				<span class="font-bold text-xs">{developers}</span>
 			</p>
 		</div>
 	</div>
