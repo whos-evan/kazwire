8946920d38e55c1ed435ac549e24a54ad2e3cb22
Biune <you@example.com>
2023-01-12 14:47:53 -0600

    Contibuting.

--- src/components/faq.svelte
+++ src/components/faq.svelte
@@ -59,7 +59,7 @@
 			<p>
 				We would also like to thank the following people for contributing to the site:
 				<br>
-				<span class="font-bold text-sm">nev#0069 for providing domain(s), Blazer for creating content about the site, and to everyone who suggested games on our Discord. If you would like to see your name here just contribute to the site!</span>
+				<span class="font-bold text-sm">nev#0069 for providing domain(s), <a class="hover:underline" href="https://www.youtube.com/watch?v=qtC_-8FRgzM">Blazer</a> for creating content about the site, and to everyone who suggested games on our Discord. If you would like to see your name here just contribute to the site!</span>
 			</p>
 		</div>
 	</div>
