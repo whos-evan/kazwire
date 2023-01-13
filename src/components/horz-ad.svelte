0522930696a0948a0016ce9aa230c038905ffd90
Biune <you@example.com>
2023-01-13 17:36:40 -0600

    Undo ad changes.

--- src/components/horz-ad.svelte
+++ src/components/horz-ad.svelte
@@ -1,12 +1,13 @@
 <script>
 	import { onMount } from 'svelte';
-	let adblock = false;
-
 	onMount(() => {
 		var isAdsenseNotLoaded =
 			typeof adsbygoogle === 'undefined' || typeof adsbygoogle.loaded === 'undefined';
 		if (isAdsenseNotLoaded) {
-			adblock = true;
+			document.getElementById('GoogleAds').style.cssText =
+				'display:block !important; padding: 20px !important;';
+			document.getElementById('GoogleAds').innerHTML =
+				'<img src="/logo.png" width="75"/><p class="text-2xl text-white">Please disable your ad blocker to help support the site and keep it lightning fast!</p>';
 		} else {
 			(adsbygoogle = window.adsbygoogle || []).push({});
 		}
@@ -15,30 +16,24 @@
 			var ad = document.querySelector('ins.adsbygoogle');
 			// If the ad contains no innerHTML, ad blockers are at work
 			if (ad && ad.innerHTML.replace(/\s/g, '').length == 0) {
-				adblock = true;
+				document.getElementById('GoogleAds').style.cssText =
+					'display:block !important; padding: 20px !important;';
+				document.getElementById('GoogleAds').innerHTML =
+					'<img src="/logo.png" width="75"/><p class="text-2xl text-white">Please disable your ad blocker to help support the site and keep it lightning fast!</p>';
 			}
 		}, 3000);
 	});
 </script>
 
-<div class="Horz w-full align-center p-10">
-	{#if adblock == false}
-		<div id="GoogleAds" align="center" class="w-full">
-			<ins
-				class="adsbygoogle"
-				style="display:block"
-				data-ad-client="ca-pub-7648886706850999"
-				data-ad-slot="6262246352"
-				data-ad-format="auto"
-				data-full-width-responsive="true"
-			/>
-		</div>
-	{:else}
-		<div id="AlternativeAd" align="center" class="w-full">
-			<img src="/logo.png" width="75" />
-			<p class="text-2xl text-white">
-				Please disable your ad blocker to help support the site and keep it lightning fast!
-			</p>
-		</div>
-	{/if}
+<div class="Horz w-full align-center">
+	<div id="GoogleAds" align="center" class="w-full">
+		<ins
+			class="adsbygoogle"
+			style="display:block"
+			data-ad-client="ca-pub-7648886706850999"
+			data-ad-slot="6262246352"
+			data-ad-format="auto"
+			data-full-width-responsive="true"
+		/>
+	</div>
 </div>
--- src/components/vert-ad.svelte
+++ src/components/vert-ad.svelte
@@ -1,12 +1,13 @@
 <script>
 	import { onMount } from 'svelte';
-	let adblock = false;
-
 	onMount(() => {
 		var isAdsenseNotLoaded =
 			typeof adsbygoogle === 'undefined' || typeof adsbygoogle.loaded === 'undefined';
 		if (isAdsenseNotLoaded) {
-			adblock = true;
+			document.getElementById('GoogleAds').style.cssText =
+				'display:block !important; padding: 20px !important;';
+			document.getElementById('GoogleAds').innerHTML =
+				'<img src="/logo.png" width="75"/><p class="text-2xl text-white">Please disable your ad blocker to help support the site and keep it lightning fast!</p>';
 		} else {
 			(adsbygoogle = window.adsbygoogle || []).push({});
 		}
@@ -14,31 +15,27 @@
 			// We are targeting the first banner ad of AdSense
 			var ad = document.querySelector('ins.adsbygoogle');
 			// If the ad contains no innerHTML, ad blockers are at work
+			// if its unfilled then don't show anything
+
 			if (ad && ad.innerHTML.replace(/\s/g, '').length == 0) {
-				adblock = true;
+				document.getElementById('GoogleAds').style.cssText =
+					'display:block !important; padding: 20px !important;';
+				document.getElementById('GoogleAds').innerHTML =
+					'<img src="/logo.png" width="75"/><p class="text-2xl text-white">Please disable your ad blocker to help support the site and keep it lightning fast!</p>';
 			}
 		}, 3000);
 	});
 </script>
 
-<div class="Vert w-full h-full mt-10">
-	{#if adblock == false}
-		<div id="GoogleAds" align="center" class="w-full h-full">
-			<ins
-				class="adsbygoogle"
-				style="display:block"
-				data-ad-client="ca-pub-7648886706850999"
-				data-ad-slot="8673868840"
-				data-ad-format="vertical"
-				data-full-width-responsive="true"
-			/>
-		</div>
-	{:else}
-		<div id="AlternativeAd" align="center" class="w-full h-full">
-			<img src="/logo.png" width="75" />
-			<p class="text-2xl text-white">
-				Please disable your ad blocker to help support the site and keep it lightning fast!
-			</p>
-		</div>
-	{/if}
+<div class="Vert w-full h-full">
+	<div id="GoogleAds" align="center" class="w-full h-full">
+		<ins
+			class="adsbygoogle"
+			style="display:block"
+			data-ad-client="ca-pub-7648886706850999"
+			data-ad-slot="8673868840"
+			data-ad-format="vertical"
+			data-full-width-responsive="true"
+		/>
+	</div>
 </div>
