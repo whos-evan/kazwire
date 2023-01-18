fbfb3075f8ec82197e3e6a61e5e5e093fba73076
Biune <you@example.com>
2023-01-17 22:27:22 -0600

    Shorten wording because mobile.

--- src/routes/discord/discord.svelte
+++ src/routes/discord/discord.svelte
@@ -4,7 +4,7 @@
 	let embedURL = "https://discord.gg/ryAmV8ATNh"
 	let title = "Kazwire Discord"
 	let description = "Kazwire Discord"
-	let developer = "Kazwire -- (Do NOT join if you are under 13. Please don't go on any forum site, especially Discord, if you are under 13.)"
+	let developer = "Kazwire — (Must be 13 years or older to join)"
 </script>
 
 <div class="flex h-[calc(100vh-132px)] max-w-full pl-5 pr-5 pb-5">
