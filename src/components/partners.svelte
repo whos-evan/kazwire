f1e5968dbb2f85e9b2c818ad7ecce45c40a8f074
Biune <you@example.com>
2023-02-04 22:29:42 -0600

    Better for mobile.

--- src/components/partners.svelte
+++ src/components/partners.svelte
@@ -1,12 +1,18 @@
 <script>
 	import Box from './box.svelte';
-	import { onMount } from 'svelte';
+	import { onMount, onDestroy } from 'svelte';
 
 	let interval;
+	let isMobile = false;
 
 	onMount(() => {
 		let partnersScroll = document.getElementById('partnersScroll');
 		let direction = 1;
+
+		if (window.innerWidth <= 768) {
+			isMobile = true;
+			return;
+		}
 
 		setTimeout(function () {
 			interval = setInterval(function () {
@@ -37,15 +43,19 @@
 				}
 			}, 100);
 		});
+
+		onDestroy(() => {
+			clearInterval(interval);
+		});
 	});
 </script>
 
-<div class="Partners w-full pl-10 pr-10">
+<div class="Partners w-full sm:pl-0 sm:pr-0 pl-10 pr-10">
 	<h1 class="text-4xl text-white font-bold text-center mb-5">Partners</h1>
 	<div
 		id="partnersScroll"
 		class="scroll-smooth grid grid-flow-col lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-10 overflow-x-scroll px-10 py-10"
-		style="grid-template-columns: repeat(auto-fill,minmax(500px,1fr)); grid-auto-columns: minmax(500px,1fr);"
+		style="grid-template-columns: repeat(auto-fill,minmax(350px,1fr)); grid-auto-columns: minmax(350px,1fr);"
 	>
 		<Box
 			title="WebLFG"
