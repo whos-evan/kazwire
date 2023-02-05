7fe983dd3fb5310f2e374d679a6a974db4a0f61e
Biune <you@example.com>
2023-02-04 21:28:22 -0600

    change image

--- src/components/partners.svelte
+++ src/components/partners.svelte
@@ -5,7 +5,7 @@
 <div class="Partners w-full pl-10 pr-10">
 	<h1 class="text-4xl text-white font-bold text-center mb-5">Partners</h1>
 	<div
-		class="grid grid-flow-col lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-10 overflow-x-scroll px-5 py-5"
+		class="grid grid-flow-col lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-10 overflow-x-scroll px-10 py-10"
 		style="grid-template-columns: repeat(auto-fill,minmax(500px,1fr)); grid-auto-columns: minmax(500px,1fr);"
 	>
 		<Box
