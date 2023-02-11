e07f742144af2431e22f8c53d1743cec5999e12a
Biune <you@example.com>
2023-02-11 14:02:21 -0600

    Reroll button

--- src/lib/components/randomBox.svelte
+++ src/lib/components/randomBox.svelte
@@ -3,47 +3,53 @@
 	import gamesJson from '../../routes/games/games.json';
 	import { onMount } from 'svelte';
 	import { auth, db } from '../firebase';
- 
-	let randomGame1 = { name: 'Loading...', image: 'loading', description: '', id: '' };
-	let randomGame2 = { name: 'Loading...', image: 'loading', description: '', id: '' };
+
 	let lovedIds = [];
 	let lovedGames = [];
- 
-	onMount(() => {
-		let allGames = gamesJson['games'];
- 
+	let randomGame1, randomGame2;
+	let allGames = gamesJson['games'];
+	let loadingHearts = true;
+
+	function rerollGames() {
 		randomGame1 = allGames[Math.floor(Math.random() * allGames.length)];
 		randomGame2 = allGames[Math.floor(Math.random() * allGames.length)];
 		// if they are the same game, reroll
 		while (randomGame1['id'] == randomGame2['id']) {
 			randomGame2 = allGames[Math.floor(Math.random() * allGames.length)];
 		}
- 
-		const user = auth.currentUser;
-		if (user) {
-			db.collection('users')
-				.doc(user.uid)
-				.get()
-				.then((doc) => {
-					if (doc.exists) {
-						lovedIds = doc.data().lovedGames;
-						lovedGames = allGames.filter((game) => lovedIds.includes(game['id']));
-						lovedGames.sort((a, b) => a['id'] - b['id']);
-					} else {
-						let loves = localStorage.getItem('loved') || '';
-						lovedIds = loves.split(',').filter((item) => item !== '');
-						lovedGames = allGames.filter((game) => lovedIds.includes(game['id']));
-						lovedGames.sort((a, b) => a['id'] - b['id']);
-					}
-				});
-		} else {
-			let loves = localStorage.getItem('loved') || '';
-			lovedIds = loves.split(',').filter((item) => item !== '');
+	}
+
+	onMount(() => {
+		randomGame1 = allGames[Math.floor(Math.random() * allGames.length)];
+		randomGame2 = allGames[Math.floor(Math.random() * allGames.length)];
+		// if they are the same game, reroll
+		while (randomGame1['id'] == randomGame2['id']) {
+			randomGame2 = allGames[Math.floor(Math.random() * allGames.length)];
+		}
+
+		// Check if the user is logged in
+		auth.onAuthStateChanged(async (user) => {
+			if (user) {
+				loadingHearts = true;
+				// Fetch the user's data from Firestore
+				const userData = await db.collection('users').doc(user.uid).get();
+				lovedIds = userData.data()?.lovedGames;
+				if (lovedIds == undefined) {
+					loadingHearts = false;
+					return;
+				}
+			} else {
+				// Fetch the loved games from local storage
+				let loves = localStorage.getItem('lovedGames') || '';
+				lovedIds = JSON.parse(loves);
+			}
 			lovedGames = allGames.filter((game) => lovedIds.includes(game['id']));
 			lovedGames.sort((a, b) => a['id'] - b['id']);
-		}
+			loadingHearts = false;
+		});
 	});
 </script>
+
 <div class="text-white w-full text-center">
 	<h1 class="lg:text-5xl md:text-4xl sm:text-xl font-bold">
 		{#if lovedGames.length > 1}
@@ -52,12 +58,25 @@
 			Here is your loved game!
 		{:else}
 			Here are two random games!
+			<button
+				class="bg-secondaryLight dark:bg-secondaryDark hover:bg-sky-700 dark:hover:bg-sky-700 font-bold transition ease-in-out duration-300 hover:drop-shadow-none drop-shadow-[5px_5px_0_rgba(1,55,84,1)] pr-5 pl-5 pt-3 pb-3 mt-5 mr-5 rounded-lg text-lg hover:scale-[105%]"
+				on:click={rerollGames}
+			>
+				Reroll 🎰
+			</button>
 		{/if}
 	</h1>
-		<div
+
+	<div
 		class="grid grid-flow-rows lg:grid-cols-2 lg:grid-rows-none sm:grid-cols-1 sm:grid-rows-2 auto-rows-auto mt-10 gap-10"
 	>
-		{#if lovedGames.length == 0}
+		{#if loadingHearts}
+			<div class="flex justify-center">
+				<div
+					class="col-start-1 animate-spin rounded-full ml-auto h-32 w-32 border-b-2 border-white"
+				/>
+			</div>
+		{:else if lovedGames.length == 0}
 			<Box
 				title={randomGame1['name']}
 				image={randomGame1['image']}
@@ -98,6 +117,6 @@
 				category="Loved"
 				color="red"
 			/>
-			{/if}
-		</div>
+		{/if}
+	</div>
 </div>
