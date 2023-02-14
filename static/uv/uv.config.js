f1c91b6955e810cd2199a73fe29bd476c961ec20
whs-evan <72959444+whos-evan@users.noreply.github.com>
2023-02-14 10:15:04 -0600

    random bare-server

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,7 +1,13 @@
+// get random bare-server url
+var urls = [
+	'https://uv.kazwire.com/bare/',
+	'https://uv2.kazwire.com/bare/'
+];
+
 /*global Ultraviolet*/
 self.__uv$config = {
 	prefix: '/search/',
-	bare: '/bare/',
+	bare: urls[Math.floor(Math.random() * urls.length)],
 	encodeUrl: Ultraviolet.codec.xor.encode,
 	decodeUrl: Ultraviolet.codec.xor.decode,
 	handler: '/uv/uv.handler.js',
