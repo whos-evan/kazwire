ece431332728afa705161e56380bc1f83a9560d0
Biune <you@example.com>
2023-02-16 22:03:46 -0600

    Remove that test bare-server.

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,14 +1,13 @@
 // get bareServer from localStorage
 var urls = [];
 // add 'uv' as a subdomain to the current domain
-// urls.push('https://uv.' + location.hostname.split('.').reverse().splice(0,2).reverse().join('.') + '/bare/');
-// urls.push('https://uv2.' + location.hostname.split('.').reverse().splice(0,2).reverse().join('.') + '/bare/');
+urls.push('https://uv.' + location.hostname.split('.').reverse().splice(0,2).reverse().join('.') + '/bare/');
+urls.push('https://uv2.' + location.hostname.split('.').reverse().splice(0,2).reverse().join('.') + '/bare/');
 
 /*global Ultraviolet*/
 self.__uv$config = {
 	prefix: '/search/',
-	// bare: urls[Math.floor(Math.random() * urls.length)],
-	bare: "https://uv2.kazwire.com/bare/",
+	bare: urls[Math.floor(Math.random() * urls.length)],
 	encodeUrl: Ultraviolet.codec.xor.encode,
 	decodeUrl: Ultraviolet.codec.xor.decode,
 	handler: '/uv/uv.handler.js',
