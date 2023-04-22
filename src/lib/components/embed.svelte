3d349bdb7d340cc2c21ddb88967b260d363a2347
Biune <you@example.com>
2023-04-22 13:48:35 -0500

    Fix mobile support

--- src/lib/components/embed.svelte
+++ src/lib/components/embed.svelte
@@ -122,13 +122,10 @@
 			if (localStorage.getItem(type)) {
 				localStorage.setItem(
 					type,
-					JSON.stringify([
-						...JSON.parse(localStorage.getItem(type) || '[]'),
-						content
-					])
+					JSON.stringify([...JSON.parse(localStorage.getItem(type) || '[]'), content])
 				);
 			} else {
-				localStorage.setItem(type, JSON.stringify([ content ]));
+				localStorage.setItem(type, JSON.stringify([content]));
 			}
 		}
 	}
@@ -177,7 +174,7 @@
 			let loved = await getFromDB('lovedGames').then((data) => {
 				return data;
 			});
-			
+
 			if (loved && loved.length > 0) {
 				for (let i = 0; i < loved.length; i++) {
 					if (loved[i] == game.id) {
@@ -437,42 +434,40 @@
 			</div>
 		{/if}
 
-		<div class="grid">
-			<div
-				class="w-full relative items-center leading-[3.5rem] mt-2 rounded-b-lg bg-secondaryLight dark:bg-secondaryDark text-white"
-			>
+		<div
+			class="w-full relative items-center leading-[3.5rem] mt-2 rounded-b-lg bg-secondaryLight dark:bg-secondaryDark text-white"
+		>
+			<div class="float-right mr-5">
+				<button class="mt-4 h-5 w-5 fill-white" on:click={fullScreen}>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
+						><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path
+							d="M32 32C14.3 32 0 46.3 0 64v96c0 17.7 14.3 32 32 32s32-14.3 32-32V96h64c17.7 0 32-14.3 32-32s-14.3-32-32-32H32zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7 14.3 32 32 32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H64V352zM320 32c-17.7 0-32 14.3-32 32s14.3 32 32 32h64v64c0 17.7 14.3 32 32 32s32-14.3 32-32V64c0-17.7-14.3-32-32-32H320zM448 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v64H320c-17.7 0-32 14.3-32 32s14.3 32 32 32h96c17.7 0 32-14.3 32-32V352z"
+						/></svg
+					>
+				</button>
+			</div>
+			<div class="float-right mr-5">
+				<button class="mt-[1.1rem] h-5 w-5 fill-white" on:click={maximize}>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
+						><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path
+							d="M344 0H488c13.3 0 24 10.7 24 24V168c0 9.7-5.8 18.5-14.8 22.2s-19.3 1.7-26.2-5.2l-39-39-87 87c-9.4 9.4-24.6 9.4-33.9 0l-32-32c-9.4-9.4-9.4-24.6 0-33.9l87-87L327 41c-6.9-6.9-8.9-17.2-5.2-26.2S334.3 0 344 0zM184 496H40c-13.3 0-24-10.7-24-24V328c0-9.7 5.8-18.5 14.8-22.2s19.3-1.7 26.2 5.2l39 39 87-87c9.4-9.4 24.6-9.4 33.9 0l32 32c9.4 9.4 9.4 24.6 0 33.9l-87 87 39 39c6.9 6.9 8.9 17.2 5.2 26.2s-12.5 14.8-22.2 14.8z"
+						/></svg
+					>
+				</button>
+			</div>
+			{#if loadHeart != false || loadHeart == undefined}
 				<div class="float-right mr-5">
-					<button class="mt-4 h-5 w-5 fill-white" on:click={fullScreen}>
-						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
-							><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path
-								d="M32 32C14.3 32 0 46.3 0 64v96c0 17.7 14.3 32 32 32s32-14.3 32-32V96h64c17.7 0 32-14.3 32-32s-14.3-32-32-32H32zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7 14.3 32 32 32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H64V352zM320 32c-17.7 0-32 14.3-32 32s14.3 32 32 32h64v64c0 17.7 14.3 32 32 32s32-14.3 32-32V64c0-17.7-14.3-32-32-32H320zM448 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v64H320c-17.7 0-32 14.3-32 32s14.3 32 32 32h96c17.7 0 32-14.3 32-32V352z"
-							/></svg
-						>
-					</button>
-				</div>
-				<div class="float-right mr-5">
-					<button class="mt-[1.1rem] h-5 w-5 fill-white" on:click={maximize}>
+					<button id="heart" class="mt-[1.1rem] h-5 w-5 fill-white" on:click={toggleLoved}>
 						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
 							><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path
-								d="M344 0H488c13.3 0 24 10.7 24 24V168c0 9.7-5.8 18.5-14.8 22.2s-19.3 1.7-26.2-5.2l-39-39-87 87c-9.4 9.4-24.6 9.4-33.9 0l-32-32c-9.4-9.4-9.4-24.6 0-33.9l87-87L327 41c-6.9-6.9-8.9-17.2-5.2-26.2S334.3 0 344 0zM184 496H40c-13.3 0-24-10.7-24-24V328c0-9.7 5.8-18.5 14.8-22.2s19.3-1.7 26.2 5.2l39 39 87-87c9.4-9.4 24.6-9.4 33.9 0l32 32c9.4 9.4 9.4 24.6 0 33.9l-87 87 39 39c6.9 6.9 8.9 17.2 5.2 26.2s-12.5 14.8-22.2 14.8z"
+								d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"
 							/></svg
 						>
 					</button>
 				</div>
-				{#if loadHeart != false || loadHeart == undefined}
-					<div class="float-right mr-5">
-						<button id="heart" class="mt-[1.1rem] h-5 w-5 fill-white" on:click={toggleLoved}>
-							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
-								><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path
-									d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"
-								/></svg
-							>
-						</button>
-					</div>
-				{/if}
-				<div class="ml-5 h-auto">
-					"{title}" by: {developer}
-				</div>
+			{/if}
+			<div class="ml-5 h-auto truncate">
+				"{title}" by: {developer}
 			</div>
 		</div>
 	</div>
