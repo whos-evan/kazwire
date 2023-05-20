b0d67864407470b9c1070e2f75413504b1f0dec1
Veracity <94334028+Veracity69@users.noreply.github.com>
2023-05-20 15:48:00 -0400

    Update search.svelte

--- src/routes/search/search.svelte
+++ src/routes/search/search.svelte
@@ -240,6 +240,119 @@
 							>
 						</button>
 					</div>
+					<div class="float-right mr-5">
+						<button class="mt-[1.1rem] h-5 w-5 fill-white" id="refresh">
+							<svg width="25px" height="25px" viewBox="0 0 512 512" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#ffffff" stroke="#ffffff" style="margin-top:-2px;">
+								<g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+								<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+								<g id="SVGRepo_iconCarrier">
+									<title>reload</title>
+									<g id="Page-1" stroke-width="19.968" fill="none" fill-rule="evenodd">
+										<g id="Combined-Shape" fill="#ffffff" transform="translate(64.000000, 64.000000)">
+											<path d="M234.666667,149.333333 L234.666667,106.666667 L314.564847,106.664112 C287.579138,67.9778918 242.745446,42.6666667 192,42.6666667 C109.525477,42.6666667 42.6666667,109.525477 42.6666667,192 C42.6666667,274.474523 109.525477,341.333333 192,341.333333 C268.201293,341.333333 331.072074,284.258623 340.195444,210.526102 L382.537159,215.817985 C370.807686,310.617565 289.973536,384 192,384 C85.961328,384 1.42108547e-14,298.038672 1.42108547e-14,192 C1.42108547e-14,85.961328 85.961328,1.42108547e-14 192,1.42108547e-14 C252.316171,1.42108547e-14 306.136355,27.8126321 341.335366,71.3127128 L341.333333,1.42108547e-14 L384,1.42108547e-14 L384,149.333333 L234.666667,149.333333 Z">
+											</path>
+										</g>
+									</g>
+								</g>
+							</svg>
+						</button>
+					</div>
+					<script>
+						document.querySelector("#refresh").onclick = function() {
+							if(!document.querySelector('#search-frame > div')){
+								document.getElementById('search-iframe').contentWindow.location.reload();
+							}
+						}
+					function He(c) {
+						let e = "https://www.google.com/search?q=%s";
+						try {
+							return new URL(c).toString()
+						} catch {}
+						try {
+							const t = new URL(`http://${c}`);
+							if (t.hostname.includes("."))
+								return t.toString()
+						} catch {}
+						return e.replace("%s", encodeURIComponent(c))
+					}
+
+					document.querySelector("#uv-address").onkeyup = () => {
+						searching(document.querySelector("#uv-address").value);
+					}
+
+					var s = document.createElement("style");
+					s.textContent = "#autofill:hover {text-decoration: underline}";
+					document.head.appendChild(s);
+
+					document.body.addEventListener('click', async (e) => {
+						if(e.target.id == "uv-address") {
+							if(document.querySelector("#uv-address").value != ""){
+								document.querySelector("#uv-address").style.borderRadius = ".5rem .5rem 0 0";
+							}
+							if(document.querySelectorAll("#autofill")){
+								for(var i=0;i<document.querySelectorAll("#autofill").length;i++){
+									document.querySelectorAll("#autofill")[i].style.display = "block";
+								}
+							}
+						} else{
+							if (e.target.id == "autofill") {
+								var f = document.getElementById("search-iframe");
+								await registerSW();
+								let c = e.target.innerText;
+								if(document.querySelector("#search-frame > div")) {
+									f.onload = () => {
+										document.querySelector("#search-frame > div").remove();
+									}
+								}
+								f.src = __uv$config.prefix + __uv$config.encodeUrl(He(c));
+							}
+							document.querySelector("#uv-address").style.borderRadius = ".5rem";
+							if(document.querySelectorAll("#autofill")){
+							for(var i=0;i<document.querySelectorAll("#autofill").length;i++){
+								document.querySelectorAll("#autofill")[i].style.display = "none";
+							}
+							}
+						}
+					});
+
+					async function searching(q) {
+					var rawResponse = await fetch('https://cors.zimjs.com/https://duckduckgo.com/ac/?q=' + q + '&type=list', {
+						method: 'POST',
+						headers: {
+						'Accept': 'application/json',
+						'Content-Type': 'application/json'
+						}
+					});
+					var l = await rawResponse.json();
+					var l = l[1];
+					document.querySelector("#uv-address").style.borderRadius = ".5rem .5rem 0 0";
+
+					if(document.querySelectorAll("#autofill")){
+						var a = document.querySelectorAll("#autofill");
+						for(var i=0;i<a.length;i++){
+							a[i].remove();
+						}
+					}
+
+					if(q != "") {
+						for(var i=0;i<l.length;i++){
+							var newEl = document.createElement("div");
+							newEl.id = "autofill";
+							newEl.className = "md:w-[35vw] sm:w-[70vw] text-black placeholder:text-gray-500";
+							newEl.style = "background:white;padding:calc(1.5rem / 2);cursor:pointer;position:absolute;left: 0; right: 0; margin-left: auto; margin-right: auto;z-index:999;";
+							newEl.style.width = document.querySelector("#uv-address").offsetWidth;
+							newEl.style.marginTop = 2.5*i + "rem";
+							if(i >= l.length - 1){
+								newEl.style.borderRadius = "0 0 .5rem .5rem";
+							}
+							newEl.innerText = l[i];
+							document.querySelector("#uv-form").appendChild(newEl);
+						}
+					} else {
+						document.querySelector("#uv-address").style.borderRadius = ".5rem";
+					}
+					}
+					</script>
 					<div id="title" class="ml-5">Nothing here...</div>
 				</div>
 			</div>
