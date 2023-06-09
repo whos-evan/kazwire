1548dbff4483c0e01c5f8031fc934fd3e2cc5916
whos-evan <test@gmail.com>
2023-06-09 15:17:44 -0500

    Tab cloaking support.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,49 @@
 <script lang="ts">
 	import '../app.css';
+	import { page } from '$app/stores';
 	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
 	import Icon from '@iconify/svelte';
+
+	// Reset the layout for certain pages containing the url path
+	const layoutResetPaths = ['/games/ruffle', '/games/emulator'];
+	let resetLayout = false;
+	for (const path of layoutResetPaths) {
+		if ($page.url.pathname.includes(path)) {
+			resetLayout = true;
+		}
+	}
+
+	// Function for cloaking the tab
+	function getTitle() {
+		const storedTitle = localStorage.getItem('title');
+		if (storedTitle) {
+			document.title = storedTitle;
+		}
+	}
+
+	function getFavicon() {
+		const storedFavicon: string = localStorage.getItem('favicon') || '';
+		console.log(storedFavicon);
+		if (storedFavicon) {
+			// Change favicon by creating a new link element
+			let link = document.querySelector("link[rel~='icon']") as HTMLLinkElement;
+			if (!link) {
+				link = document.createElement('link');
+				link.rel = 'icon';
+				link.type = 'image/x-icon';
+				link.href = storedFavicon;
+				document.getElementsByTagName('head')[0].appendChild(link);
+			} else {
+				link.href = storedFavicon;
+			}
+		}
+	}
+
+	onMount(() => {
+		getTitle();
+		getFavicon();
+	});
 
 	let darkMode = true;
 
@@ -37,154 +79,176 @@
 
 <svelte:window bind:innerWidth={windowWidth} />
 
-<div class="bg-primary dark:bg-black">
-	<div class="fixed z-20 w-full px-5 py-5">
-		<nav
-			class="flex w-full flex-wrap items-center justify-between rounded-lg bg-secondary bg-opacity-90 px-4 py-4 text-lg text-white shadow-md shadow-black dark:shadow-white md:py-0"
-		>
-			<div class="align-items-center mb-auto mt-auto flex-col">
-				<a href="/">
-					<img id="logo" src="/logo.png" class="inline-block h-16 p-3" alt="Kazwire Logo" />
-					<span
-						class="hidden whitespace-nowrap align-middle text-2xl font-semibold text-white lg:inline-block"
-						>Kazwire</span
+{#if resetLayout}
+	<slot />
+{:else}
+	<div class="bg-primary dark:bg-black">
+		<div class="fixed z-20 w-full px-5 py-5">
+			<nav
+				class="flex w-full flex-wrap items-center justify-between rounded-lg bg-secondary bg-opacity-90 px-4 py-4 text-lg text-white shadow-md shadow-black dark:shadow-white md:py-0"
+			>
+				<div class="align-items-center mb-auto mt-auto flex-col">
+					<a href="/">
+						<img id="logo" src="/logo.png" class="inline-block h-16 p-3" alt="Kazwire Logo" />
+						<span
+							class="hidden whitespace-nowrap align-middle text-2xl font-semibold text-white lg:inline-block"
+							>Kazwire</span
+						>
+					</a>
+				</div>
+
+				{#if windowWidth < 784}
+					<button
+						aria-label="Hamburger Menu Dropdown"
+						on:click={() => (menuIsHidden = !menuIsHidden)}
 					>
-				</a>
-			</div>
+						<Icon class="h-6 w-6" icon="pixelarticons:menu" />
+					</button>
+				{/if}
 
-			{#if windowWidth < 784}
-				<button
-					aria-label="Hamburger Menu Dropdown"
-					on:click={() => (menuIsHidden = !menuIsHidden)}
-				>
-					<Icon class="h-6 w-6" icon="pixelarticons:menu" />
-				</button>
-			{/if}
-
-			{#if menuIsHidden || windowWidth > 784}
-				<div class="w-full md:flex md:w-auto md:items-center" id="menu">
-					<ul class="pt-4 text-base text-white md:flex md:justify-between md:pt-0">
-						<li class="m-auto">
-							<a class="block py-2 hover:text-primary md:p-4" href="/search" data-sveltekit-reload>Search</a>
-						</li>
-						<li class="m-auto">
-							<a class="block py-2 hover:text-primary md:p-4" href="/games" data-sveltekit-reload>Games</a>
-						</li>
-						<li class="m-auto">
-							<a class="block py-2 hover:text-primary md:p-4" href="/apps" data-sveltekit-reload>Apps</a>
-						</li>
-						<li class="m-auto">
-							<a class="block py-2 hover:text-primary md:p-4" href="/settings" data-sveltekit-reload>Settings</a>
-						</li>
-						<li class="m-auto">
-							<a class="block py-2 hover:text-primary md:p-4" href="/discord" data-sveltekit-reload>Support/Discord</a>
-						</li>
-						<li class="m-auto">
-							<a class="block py-2 hover:text-primary md:p-4" href="/account" data-sveltekit-reload>Sign Up</a>
-						</li>
-						<li class="m-auto">
-							<button
-								id="theme-selector"
-								class="rounded-full bg-secondary p-2 font-bold text-white transition duration-100 hover:scale-[105%]"
-								on:click={() => handleSwitchDarkMode()}
-							>
-								{#if darkMode}
-									<Icon class="h-6 w-6" icon="pixelarticons:moon" />
-								{:else}
-									<Icon class="h-6 w-6" icon="pixelarticons:sun-alt" />
-								{/if}
-							</button>
-						</li>
-					</ul>
-				</div>
-			{/if}
-		</nav>
-	</div>
-
-	{#if windowWidth > 768}
-		<padding class="flex h-28 w-full" />
-	{:else}
-		<padding class="flex h-36 w-full" />
-	{/if}
-
-	<div class="mx-8 min-h-[90vh]">
-		<slot />
-	</div>
-
-	<!-- https://flowbite.com/docs/components/footer/ -->
-	<footer class="mt-10 bg-secondary p-4 sm:p-6">
-		<div class="md:flex md:justify-between">
-			<div class="mb-6 md:mb-0">
-				<a href="https://kazwire.com/" class="mb-2 flex items-center">
-					<img src="/logo.png" class="mr-3 h-8" alt="Kazwire Logo" />
-					<span class="self-center whitespace-nowrap text-2xl font-semibold text-white"
-						>Kazwire</span
-					>
-				</a>
-				<a href="/about-the-developers" class="text-md self-center text-white hover:underline">
-					Made with ❤️ by evan
-				</a>
-			</div>
-			<div class="grid grid-cols-2 gap-8 sm:grid-cols-3 sm:gap-6">
-				<div>
-					<h2 class="mb-6 text-sm font-semibold uppercase text-white">Resources</h2>
-					<ul class="text-white">
-						<li class="mb-4">
-							<a href="/request" class="hover:underline">Request a Game</a>
-						</li>
-						<li class="mb-4">
-							<a href="/discord" class="hover:underline">Get Support</a>
-						</li>
-						<li>
-							<a href="/faq" class="hover:underline">FAQ</a>
-						</li>
-					</ul>
-				</div>
-				<div>
-					<h2 class="mb-6 text-sm font-semibold uppercase text-white">Online Services</h2>
-					<ul class="text-white">
-						<li class="mb-4">
-							<a href="/search" class="hover:underline">Search</a>
-						</li>
-						<li class="mb-4">
-							<a href="/games" class="hover:underline">Games</a>
-						</li>
-						<li>
-							<a href="/apps" class="hover:underline">Apps</a>
-						</li>
-					</ul>
-				</div>
-				<div>
-					<h2 class="mb-6 text-sm font-semibold uppercase text-white">Legal</h2>
-					<ul class="text-white">
-						<li class="mb-4">
-							<a href="/legal/privacy-policy" class="hover:underline">Privacy Policy</a>
-						</li>
-						<li class="mb-4">
-							<a href="/legal/tos" class="hover:underline">Terms &amp; Conditions</a>
-						</li>
-						<li>
-							<a href="/legal/dmca" class="hover:underline">DMCA</a>
-						</li>
-					</ul>
-				</div>
-			</div>
+				{#if menuIsHidden || windowWidth > 784}
+					<div class="w-full md:flex md:w-auto md:items-center" id="menu">
+						<ul class="pt-4 text-base text-white md:flex md:justify-between md:pt-0">
+							<li class="m-auto">
+								<a class="block py-2 hover:text-primary md:p-4" href="/search" data-sveltekit-reload
+									>Search</a
+								>
+							</li>
+							<li class="m-auto">
+								<a class="block py-2 hover:text-primary md:p-4" href="/games" data-sveltekit-reload
+									>Games</a
+								>
+							</li>
+							<li class="m-auto">
+								<a class="block py-2 hover:text-primary md:p-4" href="/apps" data-sveltekit-reload
+									>Apps</a
+								>
+							</li>
+							<li class="m-auto">
+								<a
+									class="block py-2 hover:text-primary md:p-4"
+									href="/settings"
+									data-sveltekit-reload>Settings</a
+								>
+							</li>
+							<li class="m-auto">
+								<a
+									class="block py-2 hover:text-primary md:p-4"
+									href="/discord"
+									data-sveltekit-reload>Support/Discord</a
+								>
+							</li>
+							<li class="m-auto">
+								<a
+									class="block py-2 hover:text-primary md:p-4"
+									href="/account"
+									data-sveltekit-reload>Sign Up</a
+								>
+							</li>
+							<li class="m-auto">
+								<button
+									id="theme-selector"
+									class="rounded-full bg-secondary p-2 font-bold text-white transition duration-100 hover:scale-[105%]"
+									on:click={() => handleSwitchDarkMode()}
+								>
+									{#if darkMode}
+										<Icon class="h-6 w-6" icon="pixelarticons:moon" />
+									{:else}
+										<Icon class="h-6 w-6" icon="pixelarticons:sun-alt" />
+									{/if}
+								</button>
+							</li>
+						</ul>
+					</div>
+				{/if}
+			</nav>
 		</div>
-		<hr class="my-6 border-gray-300 sm:mx-auto lg:my-8" />
-		<div class="sm:flex sm:items-center sm:justify-between">
-			<span class="text-sm text-white sm:text-center"
-				>© 2023 <a href="https://kazwire.com/" class="hover:underline">Kazwire</a>
-			</span>
-			<div class="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
-				<a href="https://discord.gg/FEkFSWRFM9" class="text-white hover:text-primary">
-					<Icon class="h-5 w-5" icon="akar-icons:discord-fill" />
-					<span class="sr-only">Discord server</span>
-				</a>
-				<a href="https://github.com/whos-evan/kazwire" class="text-white hover:text-primary">
-					<Icon class="h-5 w-5" icon="akar-icons:github-fill" />
-					<span class="sr-only">GitHub</span>
-				</a>
-			</div>
+
+		{#if windowWidth > 768}
+			<padding class="flex h-28 w-full" />
+		{:else}
+			<padding class="flex h-36 w-full" />
+		{/if}
+
+		<div class="mx-8 min-h-[90vh]">
+			<slot />
 		</div>
-	</footer>
-</div>
+
+		<!-- https://flowbite.com/docs/components/footer/ -->
+		<footer class="mt-10 bg-secondary p-4 sm:p-6">
+			<div class="md:flex md:justify-between">
+				<div class="mb-6 md:mb-0">
+					<a href="https://kazwire.com/" class="mb-2 flex items-center">
+						<img src="/logo.png" class="mr-3 h-8" alt="Kazwire Logo" />
+						<span class="self-center whitespace-nowrap text-2xl font-semibold text-white"
+							>Kazwire</span
+						>
+					</a>
+					<a href="/about-the-developers" class="text-md self-center text-white hover:underline">
+						Made with ❤️ by evan
+					</a>
+				</div>
+				<div class="grid grid-cols-2 gap-8 sm:grid-cols-3 sm:gap-6">
+					<div>
+						<h2 class="mb-6 text-sm font-semibold uppercase text-white">Resources</h2>
+						<ul class="text-white">
+							<li class="mb-4">
+								<a href="/request" class="hover:underline">Request a Game</a>
+							</li>
+							<li class="mb-4">
+								<a href="/discord" class="hover:underline">Get Support</a>
+							</li>
+							<li>
+								<a href="/faq" class="hover:underline">FAQ</a>
+							</li>
+						</ul>
+					</div>
+					<div>
+						<h2 class="mb-6 text-sm font-semibold uppercase text-white">Online Services</h2>
+						<ul class="text-white">
+							<li class="mb-4">
+								<a href="/search" class="hover:underline">Search</a>
+							</li>
+							<li class="mb-4">
+								<a href="/games" class="hover:underline">Games</a>
+							</li>
+							<li>
+								<a href="/apps" class="hover:underline">Apps</a>
+							</li>
+						</ul>
+					</div>
+					<div>
+						<h2 class="mb-6 text-sm font-semibold uppercase text-white">Legal</h2>
+						<ul class="text-white">
+							<li class="mb-4">
+								<a href="/legal/privacy-policy" class="hover:underline">Privacy Policy</a>
+							</li>
+							<li class="mb-4">
+								<a href="/legal/tos" class="hover:underline">Terms &amp; Conditions</a>
+							</li>
+							<li>
+								<a href="/legal/dmca" class="hover:underline">DMCA</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<hr class="my-6 border-gray-300 sm:mx-auto lg:my-8" />
+			<div class="sm:flex sm:items-center sm:justify-between">
+				<span class="text-sm text-white sm:text-center"
+					>© 2023 <a href="https://kazwire.com/" class="hover:underline">Kazwire</a>
+				</span>
+				<div class="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+					<a href="https://discord.gg/FEkFSWRFM9" class="text-white hover:text-primary">
+						<Icon class="h-5 w-5" icon="akar-icons:discord-fill" />
+						<span class="sr-only">Discord server</span>
+					</a>
+					<a href="https://github.com/whos-evan/kazwire" class="text-white hover:text-primary">
+						<Icon class="h-5 w-5" icon="akar-icons:github-fill" />
+						<span class="sr-only">GitHub</span>
+					</a>
+				</div>
+			</div>
+		</footer>
+	</div>
+{/if}
--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,44 +1,91 @@
 <script lang="ts">
-	// TODO: Function to change title, favicon, and preset cloaks
+	import { onMount } from 'svelte';
+
+	let presetCloaks: string = '';
+	let title: string = '';
+	let favicon: string = '';
+
+	function getTitle() {
+		const storedTitle = localStorage.getItem('title');
+		if (storedTitle) {
+			document.title = storedTitle;
+			title = storedTitle;
+		}
+	}
+
 	function changeTitle() {
-		document.title = document.getElementById('title').value;
-		localStorage.setItem('title', document.getElementById('title').value);
+		if (title === '') {
+			localStorage.removeItem('title');
+		} else {
+			localStorage.setItem('title', title);
+		}
+		getTitle();
+	}
+
+	function getFavicon() {
+		const storedFavicon: string = localStorage.getItem('favicon') || '';
+		console.log(storedFavicon);
+		if (storedFavicon) {
+			// Change favicon by creating a new link element
+			let link = document.querySelector("link[rel~='icon']") as HTMLLinkElement;
+			if (!link) {
+				link = document.createElement('link');
+				link.rel = 'icon';
+				link.type = 'image/x-icon';
+				link.href = storedFavicon;
+				document.getElementsByTagName('head')[0].appendChild(link);
+			} else {
+				link.href = storedFavicon;
+			}
+			favicon = storedFavicon;
+		} else {
+			favicon = '';
+		}
 	}
 
 	function changeFavicon() {
-		var link = document.querySelector("link[rel~='icon']");
-		if (!link) {
-			link = document.createElement('link');
-			link.rel = 'icon';
-			document.getElementsByTagName('head')[0].appendChild(link);
+		if (favicon === '') {
+			localStorage.removeItem('favicon');
+		} else {
+			localStorage.setItem('favicon', favicon);
 		}
-		link.href = document.getElementById('favicon').value;
-		localStorage.setItem('favicon', document.getElementById('favicon').value);
+		getFavicon();
 	}
 
-	let presetCloaks;
+	function getPresetCloak() {
+		const storedPresetCloak: string = localStorage.getItem('presetCloak') || '';
+		if (storedPresetCloak == 'Kazwire') {
+			title = ''
+			favicon = ''
+			changeTitle()
+			changeFavicon()
+		} else if (storedPresetCloak) {
+			presetCloaks = storedPresetCloak;
+			const options = document.getElementById('presetCloaks') as HTMLSelectElement;
+			title = options.selectedOptions[0].innerText;
+			favicon = options.value;
+			changeTitle()
+			changeFavicon()
+		} else {
+			presetCloaks = '';
+		}
+	}
 
 	function changePresetCloaks() {
-		var link = document.querySelector("link[rel~='icon']");
-		if (!link) {
-			link = document.createElement('link');
-			link.rel = 'icon';
-			document.getElementsByTagName('head')[0].appendChild(link);
+		const options = document.getElementById('presetCloaks') as HTMLSelectElement;
+		presetCloaks = options.selectedOptions[0].innerText;
+		if (presetCloaks === '') {
+			localStorage.removeItem('presetCloak');
+		} else {
+			localStorage.setItem('presetCloak', presetCloaks);
 		}
-		link.href = presetCloaks;
-		localStorage.setItem('favicon', presetCloaks);
-
-		document.title =
-			document.getElementById('presetCloaks').options[
-				document.getElementById('presetCloaks').selectedIndex
-			].text;
-		localStorage.setItem(
-			'title',
-			document.getElementById('presetCloaks').options[
-				document.getElementById('presetCloaks').selectedIndex
-			].text
-		);
+		getPresetCloak();
 	}
+
+	onMount(() => {
+		getTitle();
+		getFavicon();
+	});
 </script>
 
 <div class="mt-10 h-auto min-h-[65vh] w-full text-center text-white">
@@ -46,7 +93,7 @@
 	<div class="mt-5 grid grid-cols-1 gap-10">
 		<div class="col-start-1 row-start-1">
 			Preset Cloaks:
-			<select id="presetCloaks" class="mr-2 rounded-md p-3 text-black" bind:value={presetCloaks}>
+			<select id="presetCloaks" class="mr-2 rounded-md p-3 text-black">
 				<option value="/favicon.ico" selected>Kazwire</option>
 				<option value="https://google.com/favicon.ico">Google</option>
 				<option value="https://ssl.gstatic.com/classroom/ic_product_classroom_144.png"
@@ -55,7 +102,12 @@
 				<option value="https://drive.google.com/favicon.ico">Google Drive</option>
 				<option value="https://canvas.instructure.com/favicon.ico">Canvas</option>
 			</select>
-			<button class="rounded-xl bg-blue-600 px-4 py-2 font-bold text-white hover:bg-blue-700" on:click={changePresetCloaks}> Save </button>
+			<button
+				class="rounded-xl bg-blue-600 px-4 py-2 font-bold text-white hover:bg-blue-700"
+				on:click={changePresetCloaks}
+			>
+				Save
+			</button>
 		</div>
 		<div class="col-start-1 row-start-2">
 			Tab Cloak:
@@ -64,8 +116,14 @@
 				type="text"
 				class="mr-2 rounded-lg p-2 text-black"
 				placeholder="Enter title here..."
+				bind:value={title}
 			/>
-			<button class="rounded-xl bg-blue-600 px-4 py-2 font-bold text-white hover:bg-blue-700" on:click={changeTitle}> Save </button>
+			<button
+				class="rounded-xl bg-blue-600 px-4 py-2 font-bold text-white hover:bg-blue-700"
+				on:click={changeTitle}
+			>
+				Save
+			</button>
 		</div>
 		<div class="col-start-1 row-start-3">
 			Custom Favicon:
@@ -74,8 +132,14 @@
 				type="text"
 				class="mr-2 w-[18rem] rounded-lg p-2 text-black"
 				placeholder="https://google.com/favicon.ico"
+				bind:value={favicon}
 			/>
-			<button class="rounded-xl bg-blue-600 px-4 py-2 font-bold text-white hover:bg-blue-700" on:click={changeFavicon}> Save </button>
+			<button
+				class="rounded-xl bg-blue-600 px-4 py-2 font-bold text-white hover:bg-blue-700"
+				on:click={changeFavicon}
+			>
+				Save
+			</button>
 		</div>
 	</div>
 </div>
