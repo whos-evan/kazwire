88dc2d015f430c0d009d68ad99209c010e173681
whos-evan <test@gmail.com>
2023-06-13 16:12:42 -0500

    Fix loading box not taking up the full size.

--- src/lib/components/Box/DefaultBoxLoading.svelte
+++ src/lib/components/Box/DefaultBoxLoading.svelte
@@ -1,17 +1,17 @@
-<div class="mx-auto relative rounded-md border border-gray-300 p-4 shadow">
+<div class="relative rounded-md border border-gray-300 p-4 shadow">
 	<div class="flex animate-pulse flex-col">
 		<!-- Image -->
-		<div class="min-h-[12rem] w-full rounded-lg bg-tertiary dark:bg-tertiarydark" />
+		<div class="dark:bg-tertiarydark min-h-[12rem] w-full rounded-lg bg-tertiary" />
 		<div class="flex-1 space-y-2 py-4">
 			<!-- Title -->
-			<div class="h-8 w-3/4 rounded bg-tertiary dark:bg-tertiarydark" />
+			<div class="dark:bg-tertiarydark h-8 w-3/4 rounded bg-tertiary" />
 			<!-- Developer -->
-			<div class="h-3 w-1/4 rounded bg-tertiary dark:bg-tertiarydark" />
+			<div class="dark:bg-tertiarydark h-3 w-1/4 rounded bg-tertiary" />
 			<!-- Tags -->
 			<div class="space-y-2">
-				<div class="h-4 w-12 rounded bg-tertiary dark:bg-tertiarydark" />
-				<div class="h-4 w-full rounded bg-tertiary dark:bg-tertiarydark" />
-				<div class="h-4 w-3/4 rounded bg-tertiary dark:bg-tertiarydark" />
+				<div class="dark:bg-tertiarydark h-4 w-12 rounded bg-tertiary" />
+				<div class="dark:bg-tertiarydark h-4 w-full rounded bg-tertiary" />
+				<div class="dark:bg-tertiarydark h-4 w-3/4 rounded bg-tertiary" />
 			</div>
 		</div>
 	</div>
