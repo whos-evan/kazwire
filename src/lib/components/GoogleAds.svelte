c2bd1147dc71127c5787b7257852355489e9883f
whos-evan <test@gmail.com>
2023-06-16 20:26:47 -0500

    Align ads in the center

--- src/lib/components/GoogleAds.svelte
+++ src/lib/components/GoogleAds.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	export let slot: string;
 
-	import { onMount } from "svelte";
+	import { onMount } from 'svelte';
 	onMount(() => {
 		(window.adsbygoogle = window.adsbygoogle || []).push({});
 	});
@@ -16,15 +16,19 @@
 </svelte:head>
 
 <div class="p-4">
-	<h3 class="min-w-[14rem] text-center text-xs uppercase tracking-widest dark:text-gray-400 text-gray-600 opacity-40">
+	<h3
+		class="min-w-[14rem] text-center text-xs uppercase tracking-widest text-gray-600 opacity-40 dark:text-gray-400"
+	>
 		Advertisement
 	</h3>
-	<ins
-		class="adsbygoogle"
-		style="display:block"
-		data-ad-client="ca-pub-7648886706850999"
-		data-ad-slot={slot}
-		data-ad-format="auto"
-		data-full-width-responsive="true"
-	/>
+	<div class="flex h-full items-center justify-center">
+		<ins
+			class="adsbygoogle"
+			style="display:block"
+			data-ad-client="ca-pub-7648886706850999"
+			data-ad-slot={slot}
+			data-ad-format="auto"
+			data-full-width-responsive="true"
+		/>
+	</div>
 </div>
