7b77a971e97325fe236cc8728e5489516e985e15
whos-evan <test@gmail.com>
2023-06-16 21:17:15 -0500

    Revert

--- src/lib/components/GoogleAds.svelte
+++ src/lib/components/GoogleAds.svelte
@@ -15,20 +15,18 @@
 	></script>
 </svelte:head>
 
-<div class="p-4">
+<div class="p-4 justify-center items-center">
 	<h3
 		class="min-w-[14rem] text-center text-xs uppercase tracking-widest text-gray-600 opacity-40 dark:text-gray-400"
 	>
 		Advertisement
 	</h3>
-	<div class="flex h-full items-center justify-center">
-		<ins
-			class="adsbygoogle"
-			style="display:block"
-			data-ad-client="ca-pub-7648886706850999"
-			data-ad-slot={slot}
-			data-ad-format="auto"
-			data-full-width-responsive="true"
-		/>
-	</div>
+	<ins
+		class="adsbygoogle"
+		style="display:block"
+		data-ad-client="ca-pub-7648886706850999"
+		data-ad-slot={slot}
+		data-ad-format="auto"
+		data-full-width-responsive="true"
+	/>
 </div>
