7e580061c4465a09c46a9bc34b204cd9b1f418c3
whos-evan <test@gmail.com>
2023-06-22 17:20:15 -0500

    Change letter spacing on hero

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,16 +1,17 @@
-<div class="rounded-3xl bg-tertiary p-8 text-black dark:bg-tertiaryDark dark:text-white flex flex-col justify-center h-full">
-	<div class="text-5xl font-bold md:text-7xl">
-	  <h1>
-		<span class="block pb-1">Your favorites</span>
-		<span class="text-blue-800 dark:text-blue-400">all in one place!</span>
-	  </h1>
+<div
+	class="flex h-full flex-col justify-center rounded-3xl bg-tertiary p-8 text-black dark:bg-tertiaryDark dark:text-white"
+>
+	<div class="text-5xl font-bold tracking-tighter md:text-7xl">
+		<h1>
+			<span class="block pb-1">Your favorites</span>
+			<span class="text-blue-800 dark:text-blue-400">all in one place!</span>
+		</h1>
 	</div>
-	<p class="pt-2 text-md md:text-3xl">
-	  Say goodbye to slow internet speeds and hello to lightning-fast browsing with Kazwire.
+	<p class="text-md pt-2 tracking-tight md:text-3xl">
+		Say goodbye to slow internet speeds and hello to lightning-fast browsing with Kazwire.
 	</p>
 	<div class="flex flex-col gap-3 pt-8 text-center sm:flex-row sm:text-left">
-	  <a href="/search" class="btn-lg">Start Browsing</a>
-	  <a href="/games" class="btn-lg">Start Playing</a>
+		<a href="/search" class="btn-lg">Start Browsing</a>
+		<a href="/games" class="btn-lg">Start Playing</a>
 	</div>
-  </div>
-  
+</div>
