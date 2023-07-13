a73a09cd1764fb11722c6beefc2e97e363a99c07
whos-evan <me@example.com>
2023-07-13 15:25:23 -0500

    change colors for faq

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -13,15 +13,15 @@
 <Horz />
 
 <div class="min-h-[70vh]">
-	<h1 class="text-4xl font-bold text-white">FAQ</h1>
-	<p class="font-medium text-white">
+	<h1 class="text-4xl font-bold text-black dark:text-white">FAQ</h1>
+	<p class="font-medium text-black dark:text-white">
 		Here you can find all the frequently asked questions about the site.
 	</p>
 	<div class="grid-flow-rows mt-5 grid auto-rows-auto gap-10 sm:grid-cols-1 md:grid-cols-2">
 		{#each faqJSON['faq'] as faq}
-			<div class="rounded-md bg-secondary p-5">
-				<h1 class="text-2xl font-bold text-white">{faq['question']}</h1>
-				<p class="break-words font-medium text-white">{faq['answer']}</p>
+			<div class="rounded-md bg-tertiary p-5 dark:bg-tertiaryDark">
+				<h1 class="text-2xl font-bold text-black dark:text-white">{faq['question']}</h1>
+				<p class="break-words font-medium text-black dark:text-white">{faq['answer']}</p>
 			</div>
 		{/each}
 	</div>
