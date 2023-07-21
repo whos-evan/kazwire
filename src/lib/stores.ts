dbc85dc171f10f4fdd2b5c7ab8448156c02d48fc
whos-evan <test@gmail.com>
2023-07-21 00:35:38 -0500

    revert

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,13 +1,4 @@
-import { browser } from '$app/environment';
 import { writable } from 'svelte/store';
 
 export const darkMode = writable(false);
 export const isLiked = writable(false);
-
-export const neverShowSchoolRescue = writable(false);
-// Update the store with data from local storage
-if (browser) {
-	if (localStorage.getItem('neverShowSchoolRescue') === 'true') {
-		neverShowSchoolRescue.set(true);
-	}
-}
