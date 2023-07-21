68bfaa1a1f372147c96f7989887102e000c1953a
whos-evan <test@gmail.com>
2023-07-21 00:38:13 -0500

    Revert "remove very specific things"
    This reverts commit 09ca7779b141dc10f0b0f2ec750817c88a4f4a45.

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,4 +1,13 @@
+import { browser } from '$app/environment';
 import { writable } from 'svelte/store';
 
 export const darkMode = writable(false);
 export const isLiked = writable(false);
+
+export const neverShowSchoolRescue = writable(false);
+// Update the store with data from local storage
+if (browser) {
+	if (localStorage.getItem('neverShowSchoolRescue') === 'true') {
+		neverShowSchoolRescue.set(true);
+	}
+}
