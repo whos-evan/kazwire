448b0faddd5919f38860f95e9d6f9d568fc49db1
whos-evan <test@gmail.com>
2023-08-22 23:35:12 -0500

    suggested games instead of ads experiment

--- src/routes/games/[id]/+page.svelte
+++ src/routes/games/[id]/+page.svelte
@@ -72,6 +72,10 @@
 		});
 	}
 
+	import { experiments } from '$lib/experiments';
+	let showSuggestedGames: boolean = false;
+
+	import SmallBox from '$lib/components/Box/SmallBox.svelte';
 	onMount(() => {
 		registerServiceWorker();
 
@@ -81,6 +85,15 @@
 		} else {
 			isLiked.set(false);
 		}
+
+		experiments.fetchOrCreateExperimentData(
+			'showSuggestedGames',
+			'2023-09-01',
+			'Shows suggested games instead of ads.',
+			25,
+			true
+		);
+		showSuggestedGames = experiments.shouldShow('showSuggestedGames');
 	});
 
 	// Fullscreen the iframe
@@ -186,7 +199,7 @@
 		console.log(e);
 		setTimeout(() => {
 			// grab the current cursor position
-			
+
 			// Check if the button is still being hovered over by grabbing the event x, y and comparing it to the current x, y
 			if (e.x == mousePos.x && e.y == mousePos.y) {
 				// Move the button to the bottom right
@@ -240,7 +253,41 @@
 		class="float-left flex h-[calc(80vh-132px)] pb-5 sm:w-full md:w-[820px] lg:w-[1000px] xl:w-full"
 	>
 		{#if innerWidth > 1224}
-			<Vert />
+			{#if showSuggestedGames}
+				{#await suggest.Games() then suggestedGames}
+					{#if suggestedGames.length > 0}
+						<div class="mx-4">
+							<div class="flex flex-row">
+								<h1 class="mb-2 text-xl font-bold text-black dark:text-white">Suggested Games</h1>
+								<Icon
+									icon="mdi:controller"
+									class="ml-1 mt-[3px] text-2xl text-green-500 transition hover:text-blue-500"
+								/>
+							</div>
+							<div class="flex flex-col">
+								<!-- Randomly sort then choose 3 -->
+								{#each suggestedGames.sort(() => Math.random() - 0.5).slice(0, 3) as game}
+									<!-- Show boxes on top of each other vertially -->
+									<SmallBox
+										image={'/game/img/' + game.image}
+										name={game.name}
+										developer={game.developer}
+										link={'/games/' + game.id}
+										popular={game.popular || false}
+										errorMessage={game.errorMessage || undefined}
+										platformSupport={game.platform}
+										GA_EVENT="click_suggested_game"
+									/>
+								{/each}
+							</div>
+						</div>
+					{:else}
+						<Vert />
+					{/if}
+				{/await}
+			{:else}
+				<Vert />
+			{/if}
 		{/if}
 		<div class="align-center mb-14 flex-grow">
 			<div id="frame" class="h-full w-full rounded-t-lg bg-white">
