3ea34b80d8edd8d6f999710b8b81c62e24283ed2
whos-evan <test@gmail.com>
2023-09-17 18:44:56 -0500

    change UV

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,7 +1,7 @@
 /*global Ultraviolet*/
 self.__uv$config = {
     prefix: '/service/',
-    bare: '/bare/',
+    bare: 'https://kazwire.com/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
