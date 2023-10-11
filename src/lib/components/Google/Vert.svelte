8256f6e2ecbaa858aa7bfa66900bc5d5b725353b
whos-evan <test@gmail.com>
2023-10-10 21:14:51 -0500

    test

--- src/lib/components/Google/Vert.svelte
+++ src/lib/components/Google/Vert.svelte
@@ -6,14 +6,6 @@
 	});
 </script>
 
-<svelte:head>
-	<script
-		async
-		src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7648886706850999"
-		crossorigin="anonymous"
-	></script>
-</svelte:head>
-
 <div class="items-center justify-center p-4 [text-align-last:center]">
 	<h3
 		class="min-w-[14rem] text-center text-xs uppercase tracking-widest text-gray-600 opacity-40 dark:text-gray-400"
