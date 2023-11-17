71b49bafbca4dabb1161fd6e7fba2ea101fb9e1a
3van <a@a.com>
2023-11-17 10:51:22 -0600

    JOIN THE MOTHERFUCKIN FIGHT

--- src/lib/components/Google/Leaderboard.svelte
+++ src/lib/components/Google/Leaderboard.svelte
@@ -1,8 +1,27 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
+	let showAlternate: boolean = false;
+	let imgURLs = ['discord-leaderboard.png'];
+	let imgURL = imgURLs[Math.floor(Math.random() * imgURLs.length)];
+
 	onMount(() => {
 		(window.adsbygoogle = window.adsbygoogle || []).push({});
+
+		// Show alternative message if the ad isn't loaded
+		setTimeout(() => {
+			let element: HTMLElement | null = document.getElementById('Vert');
+			if (element.childElementCount == 0) {
+				showAlternate = true;
+				element.remove();
+			}
+
+			// if data-ad-status="unfilled" then show alternate
+			if (element.getAttribute('data-ad-status') == 'unfilled') {
+				showAlternate = true;
+				element.remove();
+			}
+		}, 4000);
 	});
 </script>
 
@@ -18,4 +37,10 @@
 		data-ad-client="ca-pub-7648886706850999"
 		data-ad-slot="4023379916"
 	/>
+
+	{#if showAlternate}
+		<a href="/discord" target="_blank">
+			<img src={'/assets/' + imgURL} style="height: 90; margin: auto;" alt="Discord promo" />
+		</a>
+	{/if}
 </div>
