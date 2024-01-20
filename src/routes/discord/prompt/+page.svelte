861f80dc64ffc0da2cb01cc518b31b16b4d7f15b
whos-evan <example@example.com>
2024-01-20 11:21:24 -0600

    Add Discord localization for multiple languages

--- src/routes/discord/prompt/+page.svelte
+++ src/routes/discord/prompt/+page.svelte
@@ -1,19 +1,24 @@
+<script lang="ts">
+	import { isLoading, _ } from 'svelte-i18n';
+</script>
+
 <svelte:head>
 	<title>Kazwire - Discord</title>
 </svelte:head>
 
-<grid class="flex flex-col rounded-3xl bg-tertiary p-8 dark:bg-tertiaryDark gap-1">
-	<h1 class="text-left text-4xl font-bold text-black dark:text-white">Discord</h1>
-	<p class="text-md text-black dark:text-white">
-		To access our Discord you can view it through Kazwire or you can choose to visit the URL
-		directly.
-	</p>
-	<div class="mt-4 flex sm:flex-row flex-col gap-4">
-		<a href="/discord/browser" data-sveltekit-reload>
-			<button class="btn-lg"> Access through Kazwire </button>
-		</a>
-        <a href="/discord/link">
-			<button class="btn-lg"> Access Directly </button>
-		</a>
-	</div>
-</grid>
+{#if !$isLoading}
+	<grid class="flex flex-col gap-1 rounded-3xl bg-tertiary p-8 dark:bg-tertiaryDark">
+		<h1 class="text-left text-4xl font-bold text-black dark:text-white">Discord</h1>
+		<p class="text-md text-black dark:text-white">
+			{$_('pages.discord.instructions')}
+		</p>
+		<div class="mt-4 flex flex-col gap-4 sm:flex-row">
+			<a href="/discord/browser" data-sveltekit-reload>
+				<button class="btn-lg"> {$_('pages.discord.access_through_kazwire')} </button>
+			</a>
+			<a href="/discord/link">
+				<button class="btn-lg"> {$_('pages.discord.access_directly')} </button>
+			</a>
+		</div>
+	</grid>
+{/if}
